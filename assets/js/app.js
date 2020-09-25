0ad291bee6abb2891860f26439919b7f2d158b5c
Sajal353 <sajalbiswas353@gmail.com>
2020-09-25 19:30:54 +0600

    first page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,48 @@
-var typed = new Typed('#introtxt', {
-    strings: ['Coming Soon . . . Hold On!'],
-    smartBackspace: true,
-    typeSpeed: 100,
-    loop: false
+$(document).ready(function () {
+  $("#pagepiling").pagepiling({
+    menu: null,
+    direction: "vertical",
+    verticalCentered: true,
+    sectionsColor: [],
+    anchors: ["intro", "portfolio", "contact", "about"],
+    menu: "#menu",
+    scrollingSpeed: 500,
+    easing: "swing",
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      textColor: "#fff",
+      bulletsColor: "#fff",
+      position: "right",
+      tooltips: ["Sajal", "Portfolio", "Contact Me", "About"],
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: ".section",
+    animateAnchor: true,
   });
+
+});
+
+var typed = new Typed("#introtxt", {
+  strings: ["UI Designer","Frontend Web Developer"],
+  typeSpeed: 100,
+  backDelay: 1000,
+  loop: false,
+  showCursor: false
+});
+
+const showMenu = () => {
+  document.getElementById("menu").classList.add("active");
+};
+
+const hideMenu = () => {
+  document.getElementById("menu").classList.remove("active");
+};
+
+// setInterval(function(){
+//   console.log(window.scrollY);
+// }, 1000);
