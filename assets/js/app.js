6974d41018788a9df2ad140418b1c3fbc96f4977
Sajal353 <sajalbiswas353@gmail.com>
2020-10-03 05:03:31 +0600

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,3 @@
-$(document).ready(function () {
-  $("#pagepiling").pagepiling({
-    menu: null,
-    direction: "vertical",
-    verticalCentered: true,
-    sectionsColor: [],
-    anchors: ["intro", "portfolio", "contact", "about"],
-    menu: "#menu",
-    scrollingSpeed: 500,
-    easing: "swing",
-    loopBottom: false,
-    loopTop: false,
-    css3: true,
-    navigation: {
-      textColor: "#fff",
-      bulletsColor: "#fff",
-      position: "right",
-      tooltips: ["Sajal", "Portfolio", "Contact Me", "About"],
-    },
-    normalScrollElements: null,
-    normalScrollElementTouchThreshold: 5,
-    touchSensitivity: 5,
-    keyboardScrolling: true,
-    sectionSelector: ".section",
-    animateAnchor: true,
-  });
-
-});
-
-var typed = new Typed("#introtxt", {
-  strings: ["UI Designer","Frontend Web Developer"],
-  typeSpeed: 100,
-  backDelay: 1000,
-  loop: false,
-  startDelay: 2000,
-  showCursor: false
-});
-
 const showMenu = () => {
   document.getElementById("menu").classList.add("active");
 };
@@ -44,17 +6,39 @@ const hideMenu = () => {
   document.getElementById("menu").classList.remove("active");
 };
 
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-// setInterval(function(){
-//   console.log(window.scrollY);
-// }, 1000);
+$(document).ready(function () {
+  $("#pagepiling").pagepiling({
+    menu: null,
+    direction: "vertical",
+    verticalCentered: true,
+    sectionsColor: [],
+    anchors: ["intro", "portfolio", "contact"],
+    menu: "#menu",
+    scrollingSpeed: 500,
+    easing: "swing",
+    loopBottom: true,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      bulletsColor: "#fff",
+      position: "right"
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 3,
+    keyboardScrolling: true,
+    sectionSelector: ".section",
+    animateAnchor: true,
+  });
+
+});
+
+var typed = new Typed("#introtxt", {
+  strings: ["an UI Designer","a Frontend Web Developer"],
+  typeSpeed: 100,
+  backDelay: 1000,
+  loop: false,
+  startDelay: 2000,
+  showCursor: false
+});
+
