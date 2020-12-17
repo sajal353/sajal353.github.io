1b729204d35ca78f07c1032184ae085c5a6c8108
Sajal353 <sajalbiswas353@gmail.com>
2020-12-17 14:18:30 +0600

    .

--- gemf/app.js
+++ gemf/app.js
@@ -7,17 +7,17 @@ const equity = document.querySelector(".eight");
 const movement = document.querySelector(".ten");
 
 logo.addEventListener("click", () => {
-    window.location.href = "/index.html";
+    window.location.href = "index.html";
 });
 
 global.addEventListener("click", () => {
-    window.location.href = "gemf/global.html";
+    window.location.href = "global.html";
 });
 
 equity.addEventListener("click", () => {
-    window.location.href = "/equity.html";
+    window.location.href = "equity.html";
 });
 
 movement.addEventListener("click", () => {
-    window.location.href = "/movement.html";
+    window.location.href = "movement.html";
 });
