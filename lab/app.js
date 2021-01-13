61ac6c19531803a41a4ba0be48f8499975d0b815
Sajal353 <sajalbiswas353@gmail.com>
2021-01-14 01:31:25 +0600

    .

--- lab/app.js
+++ lab/app.js
@@ -1,12 +1,14 @@
-const rw = window.innerWidth / 100;
-const rh = window.innerHeight / 100;
+const vw = window.innerWidth;
+const vh = window.innerHeight;
+const cw = document.querySelector("#motioncard").offsetWidth;
+const ch = document.querySelector("#motioncard").offsetHeight;
 
 const motionpath = {
     feature1: {
         curviness: 1,
         autoRotate: false,
         values: [
-            { x: -(50 * rw), y: 0, rotation: 270 },
+            { x: -(vw - 64 - cw), y: 0, rotation: 270 },
         ]
     },
     feature2: {
@@ -20,22 +22,22 @@ const motionpath = {
         curviness: 1,
         autoRotate: false,
         values: [
-            { x: -(50 * rw), y: 0, rotation: 0 },
+            { x: -(vw - 64 - cw), y: 0, rotation: 0 },
         ]
     }
 };
 
 var controller = new ScrollMagic.Controller();
 
-var tween = new TimelineMax().set($("#motioncard"), { x: "-50%", y: "-50%" })
+var tween = new TimelineMax().set($("#motioncard"), { x: "0%", y: "-50%" })
     .add(TweenMax.to($(".feature1"), 2, { css: { opacity: 0 }, ease: Power1.easeInOut }))
-    .add(TweenMax.to($("#motioncard"), 2, { css: { bezier: motionpath.feature1 }, ease: Power1.easeInOut }))
+    .add(TweenMax.to($("#motioncard"), 2, { css: { bezier: motionpath.feature1 }, ease: Power0.easeInOut }))
     .add(TweenMax.to($(".feature2"), 2, { css: { opacity: 1 }, ease: Power1.easeInOut }))
     .add(TweenMax.to($(".feature2"), 2, { css: { opacity: 0 }, ease: Power1.easeInOut }))
-    .add(TweenMax.to($("#motioncard"), 2, { css: { bezier: motionpath.feature2 }, ease: Power1.easeInOut }))
+    .add(TweenMax.to($("#motioncard"), 2, { css: { bezier: motionpath.feature2 }, ease: Power0.easeInOut }))
     .add(TweenMax.to($(".feature3"), 2, { css: { opacity: 1 }, ease: Power1.easeInOut }))
     .add(TweenMax.to($(".feature3"), 2, { css: { opacity: 0 }, ease: Power1.easeInOut }))
-    .add(TweenMax.to($("#motioncard"), 2, { css: { bezier: motionpath.feature3 }, ease: Power1.easeInOut }))
+    .add(TweenMax.to($("#motioncard"), 2, { css: { bezier: motionpath.feature3 }, ease: Power0.easeInOut }))
     .add(TweenMax.to($(".feature4"), 2, { css: { opacity: 1 }, ease: Power1.easeInOut }));
 
 var scene = new ScrollMagic.Scene({ triggerElement: "#motion", duration: (window.innerHeight * 8), offset: 0, triggerHook: 0 })
